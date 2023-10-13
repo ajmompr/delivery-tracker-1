2c5c7b41dd4709e084249dc4898c1fd62a4232cb
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-10-13 18:30:09 +0000

    added arrived column to deliveries table

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -3,6 +3,7 @@
 # Table name: deliveries
 #
 #  id                    :integer          not null, primary key
+#  arrived               :boolean
 #  description           :string
 #  details               :text
 #  supposed_to_arrive_on :date
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_13_182440) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_13_182857) do
   create_table "deliveries", force: :cascade do |t|
     t.string "description"
     t.date "supposed_to_arrive_on"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_13_182440) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.boolean "arrived"
   end
 
   create_table "users", force: :cascade do |t|
